a4ef3b738f97be449043d9cf6ce4eb7f2bafa118
HENWARTO <40002445+HENAWARTO@users.noreply.github.com>
2025-08-20 00:01:44 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// GitHub user-site = root domain, so base is "/"
 export default defineConfig({
   plugins: [react()],
   base: '/'
