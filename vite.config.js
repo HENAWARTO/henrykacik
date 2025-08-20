acd88cdeb13510a7d9e19f5612b6afe3b501b09f
HENWARTO <40002445+HENAWARTO@users.noreply.github.com>
2025-08-20 00:25:31 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// For a project site at /henrykacik/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/henrykacik/'
 })
